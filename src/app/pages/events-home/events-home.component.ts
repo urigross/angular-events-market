8734d7f7e9d4626cb55d112aceb1ff7c8ab56ba3
urigross <urigross@gmail.com>
2022-07-11 22:03:48 +0300

    Added events-home page. Made a route changes to it

---
+++ src/app/pages/events-home/events-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-events-home',
+  templateUrl: './events-home.component.html',
+  styleUrls: ['./events-home.component.scss']
+})
+export class EventsHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/services/app-routing.module.ts
+++ src/app/services/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventsHomeComponent } from '../pages/events-home/events-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: EventsHomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
