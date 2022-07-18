689c179ad165f2e8e214735674b300280bf16945
urigross <urigross@gmail.com>
2022-07-18 12:19:32 +0300

    Added opening to home-page

---
+++ src/app/components/header-components/header-opening/header-opening.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-opening',
+  templateUrl: './header-opening.component.html',
+  styleUrls: ['./header-opening.component.scss']
+})
+export class HeaderOpeningComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
