7f461367bbb5875c24273aec9406020d4629601c
urigross <urigross@gmail.com>
2022-07-21 10:57:45 +0300

    test with posts2 with new functions on http service and apps to show data on iphones

--- src/app/pages/events-home/events-home.component.ts
+++ src/app/pages/events-home/events-home.component.ts
@@ -11,28 +11,28 @@ import { HttpService } from 'src/app/services/http-service';
   styleUrls: ['./events-home.component.scss']
 })
 export class EventsHomeComponent implements OnInit {
-
+  
   public summerEvents: SummerEvent[] = [];
-  // public posts: Post[] = [];
-  // public posts =new Array<any>();
+  public posts: Post[] = [];
+  public posts2 =new Array<any>();
 
   constructor(private httpService: HttpService, private filterService: FilterService) { }
 
   ngOnInit(): void {
     this.getSummerEvents();
-
+    this.httpService.getPosts2().subscribe(response => {
+      this.posts2 = response;
+  });
+    
   }
-  getSummerEvents(): void {
-    this.httpService.getPosts().subscribe(response => {
-      // this.posts = response;
+  getSummerEvents(): void{
+    this.httpService.getPosts()
+    .subscribe(posts => {
+      this.posts = posts;
+      this.summerEvents = this.httpService.getSummerEvents(posts);
       this.summerEvents = this.filterService.getFutureSummerEvents(this.summerEvents);
-      this.summerEvents = this.httpService.getSummerEvents(response);
       this.summerEvents = this.httpService.getDateSortedPosts(this.summerEvents);      // Sort them by date.
     });
-    // this.httpService.getPosts()
-    // .subscribe(posts => {
-    //     this.posts = posts;
-    //   });
   }
 
 
