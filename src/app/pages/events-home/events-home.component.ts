e9447edc80a3a4d167e4a75b4295d97e66a76dd4
urigross <urigross@gmail.com>
2022-07-21 15:01:49 +0300

    Iphones views are ok now

--- src/app/pages/events-home/events-home.component.ts
+++ src/app/pages/events-home/events-home.component.ts
@@ -11,28 +11,29 @@ import { HttpService } from 'src/app/services/http-service';
   styleUrls: ['./events-home.component.scss']
 })
 export class EventsHomeComponent implements OnInit {
-  
+
   public summerEvents: SummerEvent[] = [];
   public posts: Post[] = [];
-  public posts2 =new Array<any>();
+  public posts2 = new Array<any>();
 
   constructor(private httpService: HttpService, private filterService: FilterService) { }
 
   ngOnInit(): void {
-   // this.getSummerEvents();
+    // this.getSummerEvents();
     this.httpService.getPosts2().subscribe(response => {
       this.posts = response;
       this.summerEvents = this.httpService.getSummerEvents(response);
-  });
-    
-  }
-  getSummerEvents(): void{
-    this.httpService.getPosts()
-    .subscribe(posts => {
-      this.posts = posts;
       this.summerEvents = this.filterService.getFutureSummerEvents(this.summerEvents);
       this.summerEvents = this.httpService.getDateSortedPosts(this.summerEvents);      // Sort them by date.
     });
+
+    
+  }
+  getSummerEvents(): void {
+    this.httpService.getPosts()
+      .subscribe(posts => {
+        this.posts = posts;
+      });
   }
 
 
